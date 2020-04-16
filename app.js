30c6364fb317bb75b21553069ed2541f523147ea
markus.koost <markuskoost@gmail.com>
2020-04-16 17:07:31 +0300

    Leht
    Co-Authored-By: tonysillart <tonysillart@users.noreply.github.com>

--- app.js
+++ app.js
@@ -22,13 +22,24 @@ fetch('https://ghibliapi.herokuapp.com/films')
             const h1 = document.createElement('h1')
             h1.textContent = film.title
 
+            const release = document.createElement('release')
+            release.textContent = "Release Year: " + film.release_date
+
+            const score = document.createElement('score')
+            score.textContent = "Score: " + film.rt_score
+
+            const director = document.createElement('director')
+            director.textContent = "Directed by: " + film.director
+
             const p = document.createElement('p')
             film.description = film.description.substring(0, 300)
             p.textContent = `${film.description}...`
 
             container.appendChild(card)
-
             card.appendChild(h1)
+            card.appendChild(release)
+            card.appendChild(score)
+            card.appendChild(director)
             card.appendChild(p)
         });
     })
